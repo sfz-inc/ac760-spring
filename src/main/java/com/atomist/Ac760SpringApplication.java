3e07d22dd6e87e1fddfe0ea97a7fcf98e70d6af9
Ryan Day <jryanday@gmail.com>
2018-04-16 18:27:29 +0000

    Initial commit from Atomist

---
+++ src/main/java/com/atomist/Ac760SpringApplication.java
@@ -0,0 +1,12 @@
+package com.atomist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ac760SpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ac760SpringApplication.class, args);
+	}
+}
